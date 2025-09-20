a56b5b92a8a84cf249ba1d273c5fd2bd24d8d625
hashirkhalil-oroblue <hashirkhalil876@gmail.com>
2025-09-21 00:38:58 +0500

    wsgi.py

--- oroblue_project/wsgi.py
+++ oroblue_project/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oroblue_project.settings')
+# Ensure the correct settings module is used (can be overridden by environment variable)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oroblue_project.settings")
+
+# Optional: log WSGI startup (helps debugging on Render)
+sys.stdout.write("WSGI application loaded with settings: {}\n".format(
+    os.environ["DJANGO_SETTINGS_MODULE"]
+))
 
 application = get_wsgi_application()
+
