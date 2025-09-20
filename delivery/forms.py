029ff43c333a7b9bb137ca8e395187e0bc4cfc50
hashirkhalil-oroblue <hashirkhalil876@gmail.com>
2025-09-20 23:29:06 +0500

    Add files via upload

---
+++ delivery/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from .models import Customer, Delivery, Transaction, BottlePrice
+
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ["name", "phone", "address", "bottles_at_site", "is_active"]
+
+
+class DeliveryForm(forms.ModelForm):
+    customer = forms.ModelChoiceField(
+        queryset=Customer.objects.all(),
+        widget=forms.Select(attrs={"class": "form-control searchable-select"})
+    )
+
+    class Meta:
+        model = Delivery
+        fields = [
+            "customer",
+            "bottles_delivered",
+            "bottles_returned",
+            "amount_received",
+        ]
+
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ["customer", "amount", "transaction_type", "description"]
+
+
+class BottlePriceForm(forms.ModelForm):
+    class Meta:
+        model = BottlePrice
+        fields = ["price_per_bottle"]
+
+
+class CustomerBalanceForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ["balance", "pending_balance"]
+
+
+class BottleUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ["bottles_at_site"]
